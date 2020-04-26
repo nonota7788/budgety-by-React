2e11d8c2858c93a4ec820bf41e37bf200939ed31
nonota7788 <f.t.fs0924@gmail.com>
2020-04-26 22:17:48 +0900

    Updates

---
+++ precache-manifest.4a6514a4dab592577e26c0a45a45ec31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a38a2bc3179b899d75efa2e6f9af9b",
+    "url": "/budgety-by-React/index.html"
+  },
+  {
+    "revision": "bbbe771b3984bec7e5d2",
+    "url": "/budgety-by-React/static/css/main.8d195ce3.chunk.css"
+  },
+  {
+    "revision": "9ee3da27d2a4362bf4ef",
+    "url": "/budgety-by-React/static/js/2.02c54d7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budgety-by-React/static/js/2.02c54d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbbe771b3984bec7e5d2",
+    "url": "/budgety-by-React/static/js/main.4ab60b90.chunk.js"
+  },
+  {
+    "revision": "d67ac421f2d688f9efd7",
+    "url": "/budgety-by-React/static/js/runtime-main.00d75dda.js"
+  }
+]);
